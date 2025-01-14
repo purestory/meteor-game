15240ae2594843f59b9e10feb4d5ee4372a33cf6
purestory <purestory@gmail.com>
2025-01-14 19:48:37 +0900

    자동 버전 업데이트 시스템 추가

--- update-version.js
+++ update-version.js
@@ -1,14 +1,24 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const packagePath = path.join(__dirname, "package.json");
+// package.json 읽기
+const packagePath = path.join(__dirname, 'package.json');
 const package = require(packagePath);
 
-const [major, minor] = package.version.split(".").map(Number);
+// 버전 분리
+const [major, minor] = package.version.split('.').map(Number);
+
+// minor 버전을 0.1 증가
 const newMinor = minor + 0.1;
 
+// 새 버전 설정 (소수점 1자리로 고정)
 package.version = `${major}.${newMinor.toFixed(1)}`;
 
+// 업데이트된 package.json 저장
 fs.writeFileSync(packagePath, JSON.stringify(package, null, 2));
 
-console.log(`버전이 ${package.version}로 업데이트되었습니다.`);
+// docs 폴더의 package.json도 업데이트
+const docsPackagePath = path.join(__dirname, 'docs', 'package.json');
+fs.writeFileSync(docsPackagePath, JSON.stringify(package, null, 2));
+
+console.log(`버전이 ${package.version}로 업데이트되었습니다.`); 
