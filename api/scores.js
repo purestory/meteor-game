ccc6eac335877f4708517ec19ebdc5510edbe183
purestory <purestory@gmail.com>
2025-01-16 14:46:29 +0900

    Fix CORS issues and update API URLs

--- api/scores.js
+++ api/scores.js
@@ -3,8 +3,8 @@ const { Client } = require('pg');
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', '*');
 
     if (req.method === 'OPTIONS') {
         res.status(200).end();
